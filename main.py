4294e18bf93ed8efa98f1dc54a422e22fad73c0d
Henrique da Gama Saczkowski <henrique1443@hotmail.com>
2022-05-23 03:07:57 -0300

    Update

--- main.py
+++ main.py
@@ -10,23 +10,34 @@ import cv2
 from matplotlib import pyplot as plt
 sys.setrecursionlimit(3000)
 
-INPUT_IMAGE =  './img/0.bmp'
+INPUT_IMAGE =  './img/2.bmp'
 ALTURA_MIN = 5
 LARGURA_MIN = 5
 N_PIXELS_MIN = 5
-THRESHOLD = 0.7
+THRESHOLD = 0.95
 
 
-def calcula_verdice(img):
-    
+def calcula_verdice(img):    
     rows, cols, channels = img.shape    
     grau_verdice = np.empty ((rows, cols, 1), np.float32)
-    for row in range(rows):
-        for column in range(cols):
-            grau_verdice[row][column] = img[row][column][1]*0.5870/sum(img[row][column])
-            # grau_verdice[row][column] = round(img[row][column][1]*0.5870/sum([img[row][column][0]*0.2989, img[row][column][1]*0.5870, img[row][column][2]*0.1140]), 2)
 
-    return grau_verdice
+    #Criar mascara
+    for i in range(rows):
+        for j in range(cols):
+            aux = max(img[i, j, 0], img[i, j, 2])
+            if img[i, j, 1] > aux:
+                grau_verdice[i, j] = 1 - (img[i, j, 1] - aux)
+            else: 
+                grau_verdice[i, j] = 1
+
+    #Remover fundo verde
+    for i in range(rows):
+        for j in range(cols):
+            if grau_verdice[i, j] < THRESHOLD:
+                img[i, j] = grau_verdice[i, j]
+               
+    cv2.imshow("imagem",grau_verdice)
+    cv2.imshow("imagemm",img)
 
 def main ():
 
@@ -42,18 +53,16 @@ def main ():
     mask_green = calcula_verdice(img_float)
     # aux = np.where(mask_green < 0.7, img_float, 0)
     # mask_green = 1 - mask_green
-    aux = mask_green*255
+    #aux = mask_green*255
     # rows, cols, channels = img.shape
     # for row in range(rows):
     #     for column in range(cols):
     #         aux[row][column][0]  = round((1-mask_green[row][column][0])*255)
-    cv2.imshow('grau_verdice', aux)    
+    #cv2.imshow('grau_verdice', aux)    
 
     # grau_verdice = np.empty ((rows, cols, 1), np.float32)
     # img=cv2.normalize(img, normalizedImg, 0, 0.7, cv2.NORM_MINMAX)
-    # cv2.imshow('Normalizada', img)    
-
-   
+    # cv2.imshow('Normalizada', img)       
 
     cv2.waitKey ()
     cv2.destroyAllWindows ()
