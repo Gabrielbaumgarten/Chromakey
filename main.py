048314b156b6e9aa712d05261d8ad7c8ed28528a
Gabriel Baumgarten <gabrielbaumgarten@hotmail.com>
2022-05-24 18:32:58 -0300

    Chroma key sem remoção de bordas verdes

--- main.py
+++ main.py
@@ -31,13 +31,37 @@ def calcula_verdice(img):
                 grau_verdice[i, j] = 1
 
     #Remover fundo verde
-    for i in range(rows):
-        for j in range(cols):
-            if grau_verdice[i, j] < THRESHOLD:
-                img[i, j] = grau_verdice[i, j]
+    # for i in range(rows):
+    #     for j in range(cols):
+    #         if grau_verdice[i, j] < THRESHOLD:
+    #             img[i, j] = grau_verdice[i, j]
+
+    
                
     cv2.imshow("imagem",grau_verdice)
-    cv2.imshow("imagemm",img)
+    # cv2.imshow("imagemm",img)
+
+
+    grau_verdice2 = np.empty ((rows, cols, 1), np.float32)
+    for i in range(rows):
+        for j in range(cols):
+            grau_verdice2[i, j] = (grau_verdice[i, j] - grau_verdice[1][1])/(grau_verdice.max() - grau_verdice[1][1])
+
+    cv2.imshow("imagem_normalizada",grau_verdice2)
+
+
+    teste = grau_verdice2 * img
+    cv2.imshow("testeFrenteChroma",teste)
+
+    fundo = cv2.imread ('./img/Capturar.png', cv2.IMREAD_COLOR)
+    fundo = cv2.resize(fundo, (grau_verdice2.shape[1],grau_verdice2.shape[0]))
+    fundo = fundo.astype(np.float32)/255
+    fundo = fundo * (1 - grau_verdice2)
+    cv2.imshow("testeFundoChroma",fundo)
+
+    chormaKey = fundo + teste
+    cv2.imshow("testeChroma",chormaKey)
+
 
 def main ():
 
@@ -51,18 +75,8 @@ def main ():
     # grau_verdice = img_float[:,:,1].reshape((img_float.shape[0],img_float.shape[1],1))
     # mask_green = np.where(grau_verdice < 0.2, img_float, 0)
     mask_green = calcula_verdice(img_float)
-    # aux = np.where(mask_green < 0.7, img_float, 0)
-    # mask_green = 1 - mask_green
-    #aux = mask_green*255
-    # rows, cols, channels = img.shape
-    # for row in range(rows):
-    #     for column in range(cols):
-    #         aux[row][column][0]  = round((1-mask_green[row][column][0])*255)
-    #cv2.imshow('grau_verdice', aux)    
+    cv2.imshow('grau_verdice', mask_green)    
 
-    # grau_verdice = np.empty ((rows, cols, 1), np.float32)
-    # img=cv2.normalize(img, normalizedImg, 0, 0.7, cv2.NORM_MINMAX)
-    # cv2.imshow('Normalizada', img)       
 
     cv2.waitKey ()
     cv2.destroyAllWindows ()
